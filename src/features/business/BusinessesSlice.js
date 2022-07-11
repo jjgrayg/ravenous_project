5b5a2c373f7cb5a9007530c828991747a35e3e75
jjgrayg <jjgrayg@hotmail.com>
2022-07-11 12:26:44 -0400

    CORS error fix attempt 1

--- src/features/business/BusinessesSlice.js
+++ src/features/business/BusinessesSlice.js
@@ -12,7 +12,7 @@ function isEmptyObject(obj){
 export const fetchBusinessesWithSearch = createAsyncThunk(
 	'businesses/getBusinesses',
 	async (searchObj) => {
-		let url = `https://api.yelp.com/v3/businesses/search?term=${searchObj.term}`;
+		let url = `http://cors-proxy.htmldriven.com/?url=https://api.yelp.com/v3/businesses/search?term=${searchObj.term}`;
 		if (searchObj.location === '') {
 			url += `&latitude=${encodeURI(searchObj.computerLocation.lat)}&longitude=${encodeURI(searchObj.computerLocation.lng)}`;
 		}
@@ -54,7 +54,7 @@ export const fetchAutofillRecommendations = createAsyncThunk(
 	'businesses/autofillRecommendations',
 	async ({term, location}) => {
 		if (term) {
-			let url = `https://api.yelp.com/v3/autocomplete?text=${term}`;
+			let url = `http://cors-proxy.htmldriven.com/?url=https://api.yelp.com/v3/autocomplete?text=${term}`;
 			if (location.lat !== undefined && location.lng !== undefined) {
 				url += `&latitude=${encodeURI(location.lat)}&longitude=${encodeURI(location.lng)}`;
 			}
